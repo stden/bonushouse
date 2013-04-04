44a3ad5e3df150ce7bd3d25a4fd333a5cf008f8e
Даниил Гусев <dangusev92@gmail.com>
2013-04-04 16:15:06 +0400

    Подарочные купоны больше не попадают в истекшие

--- bonushouse/views.py
+++ bonushouse/views.py
@@ -196,6 +196,7 @@ def cabinet(request):
         my_coupons = my_coupons.filter(is_used=True)
     elif filter == 'expired':
         my_coupons == my_coupons.filter(order__offer__end_date__lt=now(), is_gift=False)
+        messages.info(request, my_coupons)
     else:
         my_coupons = my_coupons.filter(order__offer__end_date__gt=now(),is_used=False)
     context['coupons_filter'] = filter
