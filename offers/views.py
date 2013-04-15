49a67e7db7d79d945f5b6fc86454c1ddea028312
Даниил Гусев <dangusev92@gmail.com>
2013-04-15 19:59:20 +0400

    форма доп. данных

--- offers/views.py
+++ offers/views.py
@@ -309,7 +309,7 @@ def buy_view(request, offer, cart_item=None):
             else:
                 initial = None
             buy_form = BuyOfferForm(user=request.user, offer=offer, initial=initial)
-        context['buy_form'] = buy_form
+            context['buy_form'] = buy_form
         return render_to_response('offers/buy.html', context)
 
 
