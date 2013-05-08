6ff2788e8298a751f104724da3dce36b273b7e3a
Даниил Гусев <dangusev92@gmail.com>
2013-05-08 16:12:35 +0400

    Ошибка на 2ом шаге

--- contracts/views.py
+++ contracts/views.py
@@ -160,7 +160,6 @@ def person_restruct_contract(request):
                     if key != 'src_club':
                         other_info[key] = unicode(other_info[key]).encode('cp1251')
                 other_info['type'] = request.session['type']
-                print other_info
                 #if settings.DEBUG:
                 fh_url = settings.FITNESSHOUSE_NOTIFY_URL_DEBUG
                 #else:
@@ -169,6 +168,7 @@ def person_restruct_contract(request):
                 comment = u'Переоформление договора %s на клиента %s %s  ' % (other_info['cid'], new_user.first_name, new_user.last_name)
                 transaction_info = ContractTransactionInfo()
                 transaction_info.save()
+                print other_info
                 transaction = ContractTransaction(operation_type=1, user=request.user, amount=0, transaction_date=now(), comment=comment, transaction_object=transaction_info) #@TODO: Допилить транзакции
                 transaction.save()
                 request_params = {
