ce9c7a903728c13e557f68e0cb8e629dd3763c54
Даниил Гусев <dangusev92@gmail.com>
2013-05-15 12:34:38 +0400

    Фикс проверки префиксов

--- contracts/views.py
+++ contracts/views.py
@@ -129,9 +129,6 @@ def person_restruct_contract(request):
                 elif response['?status'][0] == '3':
                     messages.info(request, 'Ваш договор уже находится в обработке.')
                     return render_to_response('contracts/contract_form.html', context)
-                elif response['?status'][0] == '0':
-                    messages.info(request, 'Договор не найден или данные не верны')
-                    return render_to_response('contracts/contract_form.html', context)
                 elif response['?status'][0] == '-2' or response['?status'][0] == '0':
                     messages.info(request, 'Договор не найден или данные неверны!')
                     return render_to_response('contracts/contract_form.html', context)
