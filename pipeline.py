8d8f09894d032b4cc378909b5b0d54c77bb7b282
Даниил Гусев <dangusev92@gmail.com>
2013-05-21 18:20:38 +0400

    print_data pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 def print_data(backend, details, response, user, is_new=False, *args,
                         **kwargs):
-    log = open(('/var/www/disaers/data/www/bonus-house.ru/log_social.txt'), 'w')
-    log.write('response   ' + response)
+    log = open('/var/www/disaers/data/www/bonus-house.ru/log_social.txt', 'w')
+    log.write('response  ' + response)
     log.write(details)
     log.write(user)
     log.close()
