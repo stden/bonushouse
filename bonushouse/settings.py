2c71ed40d43f50ed063fc819962d0e3417292248
Даниил Гусев <dangusev92@gmail.com>
2013-05-21 15:59:45 +0400

    print_data pipeline

--- bonushouse/settings.py
+++ bonushouse/settings.py
@@ -209,8 +209,8 @@ SOCIAL_AUTH_PIPELINE = (
     'social_auth.backends.pipeline.user.get_username',
     'social_auth.backends.pipeline.user.create_user',
     'social_auth.backends.pipeline.social.associate_user',
-    'social_auth.backends.pipeline.social.load_extra_data',
     'pipeline.print_data',
+    'social_auth.backends.pipeline.social.load_extra_data',
     'social_auth.backends.pipeline.user.update_user_details'
 )
 
