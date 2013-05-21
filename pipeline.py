cc8c0955ff629f7a7620675be1b57d4371c830ae
Даниил Гусев <dangusev92@gmail.com>
2013-05-21 18:23:42 +0400

    print_data pipeline

--- pipeline.py
+++
@@ -1,7 +0,0 @@
-from django.conf import settings
-def print_data(backend, details, response, user, is_new=False, *args,
-                        **kwargs):
-    print details
-    print response
-    print user
-
