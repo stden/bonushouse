0e42b096beb85cff90a64e63ad878a1e6ee4c895
Даниил Гусев <dangusev92@gmail.com>
2013-05-21 18:10:34 +0400

    print_data pipeline

--- bonushouse/settings.py
+++ bonushouse/settings.py
@@ -210,12 +210,12 @@ SOCIAL_AUTH_PIPELINE = (
     'social_auth.backends.pipeline.social.social_auth_user',
     'social_auth.backends.pipeline.associate.associate_by_email',
     'social_auth.backends.pipeline.misc.save_status_to_session',
+    'pipeline.print_data',
     'social_auth.backends.pipeline.user.get_username',
     'social_auth.backends.pipeline.user.create_user',
     'social_auth.backends.pipeline.social.associate_user',
     'social_auth.backends.pipeline.social.load_extra_data',
     'social_auth.backends.pipeline.user.update_user_details',
-    'social_auth.backends.pipeline.misc.save_status_to_session',
 )
 
 # A sample logging configuration. The only tangible logging
