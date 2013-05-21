14239c335f17be4fcaa66d52d999dd8ca373f2fc
Даниил Гусев <dangusev92@gmail.com>
2013-05-21 16:13:36 +0400

    print_data pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 def print_data(backend, details, response, user, is_new=False, *args,
                         **kwargs):
-    print details
-    print response
-    print user
+    log = open(settings.rel('log_social.txt'))
+    print >> log, response
+    print >> log, details
+    print >> log, user
+
