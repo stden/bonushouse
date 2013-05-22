ad2df856aefcd34ef7700e99b3ec68858a9d9a0e
Даниил Гусев <dangusev92@gmail.com>
2013-05-22 12:55:21 +0400

    отписка от рассылки

--- newsletter/utils.py
+++ newsletter/utils.py
@@ -7,6 +7,8 @@ import datetime
 def get_unsubscribe_hash(user):
     first_name = user.first_name
     last_name = user.last_name
-    birthdate_utc = time.mktime(user.get_profile().birth_date.timetuple())
+    birthdate_utc = ''
+    if user.birth_date:
+        birthdate_utc = time.mktime(user.get_profile().birth_date.timetuple())
     hash_str = first_name + last_name + str(birthdate_utc)
     return hashlib.md5(hash_str.encode('utf-16')).hexdigest()
