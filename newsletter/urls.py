ab28d34fb24dfc064d57281bbbbd5091ca51f4f6
Даниил Гусев <dangusev92@gmail.com>
2013-05-22 14:39:16 +0400

    отписка от рассылки

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-        url(r'unsubscribe/(?P<user_hash>[a-zA-Z0-9]+)/^$', 'newsletter.views.unsubscribe', name='newsletter_unsubscribe'),
+        url(r'unsubscribe/(?P<user_hash>\w+)/$', 'newsletter.views.unsubscribe', name='newsletter_unsubscribe'),
     )
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,33 +1,8 @@
 # -*- coding: utf-8 -*-
 # Create your views here.
 from bonushouse.models import UserProfile
-
-from django.contrib.auth.decorators import user_passes_test
-from django.contrib.auth.models import User
-from django.db import models
 from django.template import RequestContext
 from django.shortcuts import render_to_response, redirect, get_object_or_404
-from common.forms import CategoriesForm, PhotoForm
-from common.models import Categories
-from seo.forms import SeoModelMetaForm, SeoModelUrlForm
-from offers.forms import OffersForm, ProlongationOffersAdminForm
-from offers.models import Offers, ProlongationOffers, Order, AbonementOrder, AdditionalServicesOrder, MetaOrder, GiftOrder
-from partners.models import Partner, PartnerAddress, PartnersPage, ClubCardNumbers
-from partners.forms import PartnerForm, PartnerAddressForm, PartnersPageForm, ClubCardNumbersForm
-from flatpages.models import FlatPage
-from flatpages.forms import FlatPageForm
-from news.models import News
-from news.forms import NewsForm
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from administration.forms import BulkOffersForm, BulkCategoriesForm, BulkPartnersForm, BulkPagesForm, BulkBannersForm, \
-    BulkAuctionsForm, DateRangeForm, IdeaRewardForm, BulkFeedbacksForm, UserForm, BulkUsersForm, BulkPartnersPageForm, \
-    BulkClubCardNumbersForm, CallMeSubjectForm, BulkProlongationOffersForm
-from django.http import Http404, HttpResponse
-
-from django.core.urlresolvers import reverse_lazy
-
-from django.conf import settings
-
 
 
 
