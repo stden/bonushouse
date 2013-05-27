b85ec6f17d16c732d21f8e220f0f332dfba613a2
Даниил Гусев <dangusev92@gmail.com>
2013-05-28 00:54:01 +0400

    checkboxselectmultiple

--- partners/forms.py
+++ partners/forms.py
@@ -16,7 +16,7 @@ from django.contrib.auth.models import User
 
 class PartnerForm(forms.ModelForm):
 
-    admin_user = forms.ModelMultipleChoiceField(label='Пользователь-администратор',queryset=User.objects.filter(is_staff=True, is_superuser=True).order_by('username'), widget=forms.CheckboxSelectMultiple())
+    admin_user = forms.ModelMultipleChoiceField(label='Пользователь-администратор',queryset=User.objects.filter(is_staff=True).order_by('username'), widget=forms.CheckboxSelectMultiple())
 
     def save(self, commit=True):
         is_new = False
