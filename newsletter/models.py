6b53991da21e2682b09aee0ab74895f2a42e6c6e
Даниил Гусев <dangusev92@gmail.com>
2013-06-05 12:49:47 +0400

    Ошибка валидации

--- newsletter/models.py
+++ newsletter/models.py
@@ -97,7 +97,7 @@ class NewsletterEmail(models.Model):
     is_sent = models.BooleanField(default=False)
     def send(self, email):
         subject = self.subject
-        from_email = settings.DEFAULT_FROM_EMAIL
+        from_email = 'Бонус-Хаус'
         to = email
         subscribe_hash = unicode(User.objects.filter(email=email)[0].get_profile().subscribe_hash)
         context = Context({'hash': subscribe_hash})
